23c06cc719ffc65e4f505168d442ed0e6a98cf64
Keks <keks@htmlacademy.ru>
2022-03-16 09:58:13 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,17 +1,17 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomInteger(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomInteger (min, max) {
+  const from = Math.ceil(min);
+  const before = Math.floor(max);
+  return Math.floor(Math.random() * (before - from + 1)) + from;
 }
 
 getRandomInteger(4, 16);
 
 // с использованием https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
-function getRandomIntegerFloat(min, max, length) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return ((Math.random() * (max - min + 1)) + min).toFixed(length);
+function getRandomIntegerFloat (min, max, length) {
+  const from = Math.ceil(min);
+  const before = Math.floor(max);
+  return ((Math.random() * (before - from + 1)) + from).toFixed(length);
 }
 
 getRandomIntegerFloat(4, 19, 6);
